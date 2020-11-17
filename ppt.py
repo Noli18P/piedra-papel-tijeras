4e4723af1266bf976dcc4ce30093e818079b2e4a
Tu Nombre <investingmtz@gmail.com>
2020-11-17 15:29:04 -0600

    Arreglando imagenes para ser independientes

--- ppt.py
+++ ppt.py
@@ -1,26 +1,43 @@
-imagenes_ppt = [
+"""
+Piedra gana a tijera
+Tijera gana a papel
+Papel gana a piedra
+"""
+def papel():
+	imagen_papel = """
+	    ______
+	   |      \      
+	   |  \/  |
+	   |  /\  |
+	   |______|
 	"""
-	_____
-	|    \
-	|    |
-	|    |
-	______
-	""",
+	return imagen_papel
+
+
+def piedra():
+	imagen_piedra = """
+	    ____
+	   /    \
+	   \____/
 	"""
-	  ___
-	 /   \
-	 \___/
-	""",
+	return imagen_piedra
+
+
+def tijera():
+	imagen_tijera = """
+	   	\  _
+	 	 \/
+	 	 /\_
+	    	/ 
 	"""
-	\  _
-	 \/
-	 /\_
-	/ 
-	"""
-]
+	return imagen_tijera
+
 
 def main():
-	pass
+	imagen_tijera = tijera()
+
+	print(imagen_tijera)
+
 
 if __name__ == '__main__':
 	main()
